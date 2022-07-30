0161c0007571a150db8ba46a9aef0343668a32cc
sbx_user1051 <sbx_user1051@169.254.14.229>
2022-07-30 21:01:53 +0000

    Sat Jul 30 21:01:53 UTC 2022

--- lib/solutions/CHK/CheckoutHandlers/BundleOffersHandler.py
+++ lib/solutions/CHK/CheckoutHandlers/BundleOffersHandler.py
@@ -1,3 +1,4 @@
+import math
 from collections import defaultdict
 
 from .CheckoutHandler import CheckoutHandler
@@ -21,12 +22,14 @@ class BundleOffersHandler(CheckoutHandler):
         for sku, sku_items in items_in_specials.items():
             bundle_offer = self.bundle_offers[sku]
 
-
+            number_of_complete_bundles = math.inf
             for req_sku, count in bundle_offer['rules'].items():
-                filtered_items = filter(lambda i: i.sku==req_sku, sku_items)
+                max_offers_complete_for_sku = len(list(filter(lambda i: i.sku == req_sku, sku_items))) // count
 
+                if max_offers_complete_for_sku < number_of_complete_bundles:
+                    number_of_complete_bundles = max_offers_complete_for_sku
 
-
+            cart.total -= bundle_offer['discount'] * number_of_complete_bundles
 
             number_of_special_offers_completed = len(sku_items) // special_offer['quantity']
             number_of_items_in_offer = number_of_special_offers_completed * special_offer['quantity']
@@ -36,4 +39,5 @@ class BundleOffersHandler(CheckoutHandler):
             items_to_remove_from_cart = list(sku_items)[:number_of_items_in_offer]
 
             cart.items.difference_update(items_to_remove_from_cart)
+
 
