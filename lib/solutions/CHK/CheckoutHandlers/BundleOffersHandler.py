f64090c6d75193bb1e49e558a5946de9bd387edc
sbx_user1051 <sbx_user1051@169.254.14.229>
2022-07-30 21:27:52 +0000

    Sat Jul 30 21:27:52 UTC 2022

--- lib/solutions/CHK/CheckoutHandlers/BundleOffersHandler.py
+++ lib/solutions/CHK/CheckoutHandlers/BundleOffersHandler.py
@@ -33,6 +33,7 @@ class BundleOffersHandler(CheckoutHandler):
             cart.total -= bundle_offer['discount'] * number_of_complete_bundles
 
             for (item_sku, items_for_sku) in groupby(sku_items, key=lambda i: i.sku):
-
+                number_of_items_to_remove = number_of_complete_bundles * bundle_offer['rules'][item_sku]
                 for item in list(items_for_sku)[:number_of_complete_bundles * bundle_offer['rules'][item_sku]]:
                     CheckoutHandler.checkout_item(cart, item)
+
