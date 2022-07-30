d17fbc738df406b9012b6932997e84ca82d2cc67
sbx_user1051 <sbx_user1051@169.254.14.229>
2022-07-30 20:27:00 +0000

    Sat Jul 30 20:27:00 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,39 +1,35 @@
-
 from . import price_table
 from .models import Item
 
-class ItemFactory:
-    def load_items(item_prices: list, special_offers: dict ):
-        items_db = {}
-        for item in item_prices:
-            items_db['item'] = item['price']
 
-        return items_db
+class ItemFactory:
+
+    def __init__(self, item_prices: list):
+        self.items_db = {}
+        for item in item_prices:
+            self.items_db['item'] = item['price']
 
     def get_item(self, sku):
-        
+        return Item(sku, self.items_db[sku])
 
-items_db = load_items(price_table.item_prices, price_table.special_offers)
+
+items_factory = ItemFactory(price_table.item_prices)
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus: str) -> int:
-
-
     if not skus:
         return 0
     for sku in skus:
-        if sku not in items_db:
+        if sku not in items_factory.items_db:
             return -1
 
-
     for item in list(skus):
-        item_model = Item()
-
-
+        item_model = items_factory.get_item(item)
 
     # raise NotImplementedError()
+
 
 
 
