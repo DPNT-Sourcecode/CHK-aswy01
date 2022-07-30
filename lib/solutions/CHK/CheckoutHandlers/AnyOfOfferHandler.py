8198220c7fae09a07c5f172adfa1bd74c9a807fe
sbx_user1051 <sbx_user1051@169.254.14.229>
2022-07-30 22:17:49 +0000

    Sat Jul 30 22:17:49 UTC 2022

--- lib/solutions/CHK/CheckoutHandlers/AnyOfOfferHandler.py
+++ lib/solutions/CHK/CheckoutHandlers/AnyOfOfferHandler.py
@@ -23,12 +23,13 @@ class AnyOfOfferHandler(CheckoutHandler):
             bundle_offer = self.bundle_offers[group_id]
             bundle_quantity = bundle_offer['quantity']
 
-            number_of_complete_bundles = len(items_in_specials) // bundle_quantity
+            number_of_complete_bundles = len(sku_items) // bundle_quantity
 
             for i in list(sorted(sku_items, key=lambda e: e.price, reverse=True))[
                      :number_of_complete_bundles * bundle_quantity]:
                 cart.items.remove(i)
 
             cart.total += bundle_offer['price'] * number_of_complete_bundles
+
 
 
