4a9e1e3a4342805d530ae446cf70393c318cb1b8
sbx_user1051 <sbx_user1051@169.254.14.229>
2022-07-30 20:12:00 +0000

    Sat Jul 30 20:12:00 UTC 2022

---
+++ lib/solutions/CHK/price_table.py
@@ -0,0 +1,11 @@
+item_prices = [
+    {'A': 50},
+    {'B': 30},
+    {'C': 20},
+    {'D': 15},
+]
+
+special_offers = {
+    'A': {'quantity': 3, 'price': 130},
+    'B': {'quantity': 2, 'price': 45}
+}
---
+++ test/solution_tests/CHK/test_checkout.py
@@ -0,0 +1,23 @@
+import pytest
+
+from solutions.CHK import checkout_solution
+
+
+test_examples = [
+    ("A", 50),
+    ("B", 30),
+    ("C", 20),
+    ("D", 15),
+    ("AAA", 130),
+    ("AAAA", 180),
+    ("BB", 45),
+    ("BBB", 75)
+
+]
+
+
+class TestHello:
+
+    @pytest.mark.parametrize('item, expected_output', test_examples)
+    def test_sum(self, item, expected_output):
+        assert checkout_solution.checkout(item) == expected_output
