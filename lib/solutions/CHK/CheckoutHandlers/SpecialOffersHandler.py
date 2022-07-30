0ae286010de61cbe64cc2630cfb5ae1acede7f05
sbx_user1051 <sbx_user1051@169.254.14.229>
2022-07-30 20:36:00 +0000

    Sat Jul 30 20:36:00 UTC 2022

--- lib/solutions/CHK/CheckoutHandlers/SpecialOffersHandler.py
+++ lib/solutions/CHK/CheckoutHandlers/SpecialOffersHandler.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 from .CheckoutHandler import CheckoutHandler
 from ..models import Cart
 
@@ -8,6 +10,12 @@ class SpecialsHandler(CheckoutHandler):
         self.special_offers = special_offers
 
     def checkout_items(self, cart: Cart):
-
+        items_in_specials = defaultdict(set)
         for item in cart.items:
-            if item.sku
+            if item.sku in self.special_offers:
+                items_in_specials[item.sku].add(item)
+
+
+        for sku, sku_items in items_in_specials.items():
+
+            for special_offer in self.special_offers[sku]:
